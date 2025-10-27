d10a5a9b2f8695ed9d459c1090435651e97f92b9
Dalibor Naspalic <dalibor.naspalic@gmail.com>
2025-10-27 14:22:28 +0100

    Unit tests practice simple app

---
+++ BankUnitTesting/Bank/BankTests/BankAccountTests.cs
@@ -0,0 +1,50 @@
+﻿using Bank;
+using System.Security.Principal;
+
+namespace BankTests
+{
+    [TestClass]
+    public sealed class BankAccountTests
+    {
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // Act
+            account.Debit(debitAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+
+        [TestMethod]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount(customerName: "Mr. Bryan Walton", beginningBalance);
+
+            // Act and assert
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => account.Debit(debitAmount));
+        }
+
+        [TestMethod]
+        
+        public void Debit_WhenAmountIsMoreThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            double beginningBalance = 11.99;
+            double debitAmount = 111.99;
+            BankAccount account = new BankAccount(customerName: "Mr. Bryan Walton", beginningBalance);
+
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => account.Debit(debitAmount));
+
+        }
+    }
+}
